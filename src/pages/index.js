eb81237b259d15e6cd03d012564fac9332d9ad0f
pankaj-design <74971560+pankaj-design@users.noreply.github.com>
2023-08-22 19:14:39 +0530

    navbar initial test

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
+import Navbar from './navbar';
 
 export default function Home() {
   return (
--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,7 +1,28 @@
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>navbar________________</div>
+    <>
+    <div>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+
+        <li>
+          <Link href="/navbar">Navbar</Link>
+        </li>
+
+{/* 
+        <li>
+          <Link>
+          <a href="/home">Home</a>
+          </Link>
+        </li> */}
+      </ul>
+    </div>
+    
+    </>
   )
 }
 
