508539d8c21740b8107f2827cdd9f65a4dfa24e8
pankaj-design <74971560+pankaj-design@users.noreply.github.com>
2023-10-11 16:52:17 +0530

    navbar complete

---
+++ src/pages/customIcon.js
@@ -0,0 +1,13 @@
+// components/CustomIcon.js
+import { faCoffee } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+const CustomIcon = () => {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faCoffee} />
+    </div>
+  );
+};
+
+export default CustomIcon;
--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import Link from "next/link";
 import Image from "next/image";
+import { BiMenuAltRight } from "react-icons/bi";
 
 const Navbar = () => {
   const [navOpen, setNavOpen] = useState(true);
@@ -19,22 +20,22 @@ const Navbar = () => {
       <ul className= {`navul ${navOpen ? 'navbar-open' : 'navbar-closed'}`} >
 
         <li>
-          <Link id="nav-text" className="pose1" href="/">Home</Link>
+          <Link id="nav-text" className={` ${navOpen ? 'pose-change' : 'pose1'}`} onClick={toggleNavbar} href="/">Home</Link>
         </li>
 
         <li>
-          <Link id="nav-text" className="pose2" href="/navbar">Navbar</Link>
+          <Link id="nav-text" className={` ${navOpen ? 'pose-change' : 'pose2'}`} onClick={toggleNavbar} href="/navbar">Navbar</Link>
         </li>
         <li>
-          <Link id="nav-text" className={`pose3 ${navOpen ? 'pose-change' : 'pose3'}`} onClick={toggleNavbar} href="/quotes">Quotes</Link>
+          <Link id="nav-text" className={` ${navOpen ? 'pose-change' : 'pose3'}`} onClick={toggleNavbar} href="/quotes">Quotes</Link>
         </li>
         <li>
-          <Link id="nav-text" className="pose4 "  href="/footer">Footer</Link>
+          <Link id="nav-text" className={` ${navOpen ? 'pose-change' : 'pose4'}`} onClick={toggleNavbar}  href="/footer">Footer</Link>
         </li>
         
       </ul>
       <div className='button'  onClick={toggleNavbar}>
-         Button
+      <BiMenuAltRight size={"5rem"} color='white'/>
         </div>
     </div>
     </nav>
